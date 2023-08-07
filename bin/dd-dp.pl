67d1015229a01147571e118a6844e10923bae99e
Qiang Wang <wang-q@outlook.com>
2023-08-08 00:46:49 +0800

    Update dd-dp.pl

--- bin/dd-dp.pl
+++ bin/dd-dp.pl
@@ -24,24 +24,42 @@ dd-dp.pl - Domain distance via dynamic programming
         --help                  Help message
         --ma            FLT     Match, default is 1.0
         --mm            FLT     Mismatch, default is 0.0
-        --go            FLT     Gap opening, default is -0.01
-        --ge            FLT     Gap extension, default is -0.001
+        --gp            FLT     Gap penalty, default is -0.01
         --spe       -s  STR     Separator of fields, default is "\t"
-        --header                Inputs have header lines
+        --header    -H          Inputs have header lines
+        --dd                    Output domain distance
 
 =head1 EXAMPLE
 
     $ echo -e 'GCATGCG\nGATTACA' |
         perl dd-dp.pl --sep ""
+    GCA-T-GC-G
+    G-ATTA-CA-
 
     $ echo -e 'AAA\tFer2\tZZ\nFer2\tZZ' |
         perl dd-dp.pl
+    AAA     Fer2    ZZ
+    -       Fer2    ZZ
 
     $ echo -e 'a b c d e f g h\nb c h' |
         perl dd-dp.pl --sep " "
+    a b c d e f g h
+    - b c - - - - h
 
     $ echo -e '>seq1\na b c d e f g h\n>seq2\nb c h' |
-        perl dd-dp.pl --sep " " --header
+        perl dd-dp.pl --sep " " -H
+    >seq1
+    a b c d e f g h
+    >seq2
+    - b c - - - - h
+
+    $ echo -e '>DA\nABBC\n>R1\nBBC\n>DA\nABBC\n>R2\nAB\n>DA\nABBC\n>R3\nBB\n>DA\nABBC\n>R4\nACB\n>DA\nABBC\n>R5\nA\n' |
+        perl dd-dp.pl --sep "" -H --dd
+    >DA     >R1     1
+    >DA     >R2     2
+    >DA     >R3     2
+    >DA     >R4     3
+    >DA     >R5     3
 
 =head1 DESCRIPTION
 
@@ -57,15 +75,18 @@ L<https://gist.github.com/bibymaths/b1d649fe0ed6e641bf3948cc4d36ebe9>
 
 L<https://metacpan.org/pod/Algorithm::NeedlemanWunsch>
 
+'\t' can't be passed easily in the command line
+
 =cut
 
 Getopt::Long::GetOptions(
-    'help|?'  => sub { Getopt::Long::HelpMessage(0) },
-    'ma=f'    => \( my $opt_match       = 1.0 ),
-    'mm=f'    => \( my $opt_mismatch    = 0.0 ),
-    'go=f'    => \( my $opt_gap_penalty = -0.01 ),
-    'sep|s=s' => \( my $opt_separator   = "\t" ),
-    'header'  => \( my $opt_has_header ),
+    'help|?'   => sub { Getopt::Long::HelpMessage(0) },
+    'ma=f'     => \( my $opt_match       = 1.0 ),
+    'mm=f'     => \( my $opt_mismatch    = -0.5 ),
+    'gp=f'     => \( my $opt_gap_penalty = -0.1 ),
+    'sep|s=s'  => \( my $opt_separator   = "\t" ),
+    'header|H' => \( my $opt_has_header ),
+    'dd'       => \( my $opt_dd ),
 ) or Getopt::Long::HelpMessage(1);
 
 #----------------------------------------------------------#
@@ -75,8 +96,8 @@ Getopt::Long::GetOptions(
 while (1) {
     my $l = <>;
     last if !defined $l;
-    last if length $l == 0;
     chomp($l);
+    last if length $l == 0;
 
     my ( $sh, $th );
     my ( $s,  $t );
@@ -103,17 +124,35 @@ while (1) {
 
     my ( $sa, $ta ) = align( $mat, $s, $t );
 
-    if ( defined $opt_has_header ) {
-        print "$sh\n";
-        print join( $opt_separator, @{$sa} ), "\n";
-        print "$th\n";
-        print join( $opt_separator, @{$ta} ), "\n";
+    if ( defined $opt_dd ) {
+        my $dd = 0;
+        for my $v ( @{$sa}, @{$ta} ) {
+            if ( $v eq "-" ) {
+                $dd++;
+            }
+        }
+        if ( defined $opt_has_header ) {
+            print "$sh\t$th\t$dd\n";
+        }
+        else {
+            print "$dd\n";
+        }
     }
     else {
-        print join( $opt_separator, @{$sa} ), "\n";
-        print join( $opt_separator, @{$ta} ), "\n";
+        if ( defined $opt_has_header ) {
+            print "$sh\n";
+            print join( $opt_separator, @{$sa} ), "\n";
+            print "$th\n";
+            print join( $opt_separator, @{$ta} ), "\n";
+        }
+        else {
+            print join( $opt_separator, @{$sa} ), "\n";
+            print join( $opt_separator, @{$ta} ), "\n";
+        }
     }
 }
+
+exit;
 
 #----------------------------------------------------------#
 # Subs
